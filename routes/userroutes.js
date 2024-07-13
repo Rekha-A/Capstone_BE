b6d40b8cacc6b2d6884d8762c8720247e0a41c3e
Rekha-A <74005323+Rekha-A@users.noreply.github.com>
2024-07-13 11:33:33 +0530

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken'); // Import JWT
 const router = express.Router();
 
-const verifyToken = require('../middleware/index');
+const verifyToken = require('../middleware/main');
 
 // Import User model
 const User = require('../models/user');
